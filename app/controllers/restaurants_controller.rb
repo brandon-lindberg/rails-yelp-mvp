3b71154b125838842dcedcf13fd8a17835a260fe
brandon lindberg <lindberg.scott.brandon@gmail.com>
2021-01-12 22:09:49 +0900

    added views for index, and show

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,30 @@
 class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new([restaurant_params])
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category)
+  end
 end
