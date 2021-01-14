ffa6d5707484da66e5f01098955657920dd3fb46
brandon lindberg <lindberg.scott.brandon@gmail.com>
2021-01-14 21:22:02 +0900

    rails-yelp-mvp completed

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,23 +1,27 @@
 class ReviewsController < ApplicationController
-  before_action :findrestaurant, except: [:destroy]
 
   def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
     @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review.restaurant = @restaurant
     @review.save
     redirect_to restaurant_path(@restaurant)
   end
 
-  private
-  def find_restaurant
-    @restaurant = Restaurant.find(params[:restaurant_id])
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to restaurant_path(@review.restaurant)
   end
 
+  private
+
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :rating)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :restaurants, only: [:index, :show, :new, :create] do
-    resources :review, only: [:new, :create]
+    resources :reviews, only: [:new, :create]
   end
+
+  resources :reviews, only: [:destroy]
 end
